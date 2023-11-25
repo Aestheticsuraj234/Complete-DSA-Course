043193fd04acb580fe2b42c14de4edf3b01c09e6
Aestheticsuraj234 <jhapriya458@gmail.com>
2023-11-26 02:13:08 +0530

    Refactor linked list insertion methods

--- Chapter-3/Linked-List/linked-list.ts
+++ Chapter-3/Linked-List/linked-list.ts
@@ -1,9 +1,8 @@
 // Implementing the LinkedList
 
-// !here we are  making a class of Node which will have two properties 1. data 2. next
 class Nodes<T> {
-  public data: any;
-  public next: Node | null;
+  public data: T;
+  public next: Nodes<T> | null;
 
   constructor(data: T) {
     this.data = data;
@@ -11,12 +10,6 @@ class Nodes<T> {
   }
 }
 
-// ?1.Linked List Insertion-INSERTING A IN THE LINKED LIST AS PER THE rEQUIREMENT
-// TODO: Insertion at the beginning of the linked list✅
-// TODO: Insertion at the end of the linked list✅
-// TODO: Insertion at the given position of the linked list
-
-// !Here we are making a class of SinglyLinkedList which will have two properties 1. head 2. size
 class SinglyLinkedList<T> {
   public head: Nodes<T> | null;
   public size: number;
@@ -26,71 +19,82 @@ class SinglyLinkedList<T> {
     this.size = 0;
   }
 
-  // TODO:Insert at the beginning of the linked list
-  //   *create a new node
-  //  *check if the head is null or not
-  // *if head is null then assign the new node to head
-  // *else assign the new node to the head and assign the head to the next of the new node
-  // *increase the size of the linked list
-  // TIME COMPLEXITY: O(1)
-  // SPACE COMPLEXITY: O(1)
-
   public insertAtBeginning(data: T) {
-    let newNode = new Nodes(data);
-    // @ts-ignore
-    newNode.next = this.head;
-    this.head = newNode;
+    let newNodes = new Nodes(data);
+    newNodes.next = this.head;
+    this.head = newNodes;
   }
 
-  // TODO: INSERT AT THE END OF THE LINKED LIST
-  // *create a new node
-  // *check if the head is null or not
-  // *if head is null then assign the new node to head
-  // *else traverse the linked list till the end and assign the new node to the next of the last node
-  // *increase the size of the linked list
-  // TIME COMPLEXITY: O(n)
-  // SPACE COMPLEXITY: O(1)
   public insertAtEnd(data: T) {
-    // 1. allocate the newNode;
-    let newNode = new Nodes(data);
+    let newNodes = new Nodes(data);
 
-    // check if our linked is empty if yes then insert this new Node at the head
     if (!this.head) {
-      this.head = newNode;
+      this.head = newNodes;
+      return;
     }
 
-    // Now our newNode is pointing to the null;
-    newNode.next = null;
+    let temp = this.head;
+    while (temp.next !== null) {
+      temp = temp.next;
+    }
 
-    var temp = this.head;
-    // else check our current node is pointing ot null or not
-    //  @ts-ignore
-    while (temp !== null) temp = temp.next;
-    // @ts-ignore
-    temp.next = newNode;
-    return;
+    temp.next = newNodes;
   }
 
-  //   TODO: INSERT AT THE GIVEN POSITION OF THE LINKED LIST
-  // *create a new node
-  // *check if the head is null or not
-  // *if head is null then assign the new node to head
+  insertInMiddle(after: T, data: T): void {
+    //
+    let newNodes = new Nodes(data);
+    let temp = this.head;
 
-  insertInMiddle(after: number, data: number): void {
-    let newNode = new Nodes(data);
-    let current = this.head;
-
-    while (current && current.data !== after) {
-      // @ts-ignore
-      current = current.next;
+    while (temp !== null && temp.data !== after) {
+      temp = temp.next;
     }
 
-    if (!current) {
+    if (!temp) {
       console.log("Element not found");
       return;
     }
-    newNode.next = current.next;
-    // @ts-ignore
-    current.next = newNode;
+
+    newNodes.next = temp.next;
+    temp.next = newNodes;
   }
+
+  // !Search an element in a Linked List (Iterative and Recursive)
+
+  // *Iterative searching in a Linked List.
+
+  searchIterative(target: T): boolean {
+    let temp = this.head;
+    while (temp !== null) {
+      if (temp.data === target) {
+        return true;
+      }
+
+      temp = temp.next;
+    }
+    return false;
+  }
+
+
+  // *Recursive searching in a Linked List.
+
+    searchRecursive(temp: Nodes<T> | null, target: T): boolean {
+        if (temp === null) {
+        return false;
+        }
+    
+        if (temp.data === target) {
+        return true;
+        }
+    
+        return this.searchRecursive(temp.next, target);
+    }
+
+
 }
+
+// Implementation of the LinkedList
+
+let linkedList = new SinglyLinkedList<number>();
+
+console.log(linkedList);
